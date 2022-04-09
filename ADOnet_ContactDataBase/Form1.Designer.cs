169f075395d3d64264ae5404a6630b957025ac8d
ibozic90 <ibozic90@gmail.com>
2022-04-09 11:13:57 +0300

    Добавьте файлы проекта.

---
+++ ADOnet_ContactDataBase/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace ADOnet_ContactDataBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label17 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.UserUpdatebutton = new System.Windows.Forms.Button();
+            this.UserInsertbutton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(506, 12);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(47, 13);
+            this.label17.TabIndex = 29;
+            this.label17.Text = "BirthDay";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(509, 28);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(120, 20);
+            this.textBox4.TabIndex = 28;
+            // 
+            // UserUpdatebutton
+            // 
+            this.UserUpdatebutton.Location = new System.Drawing.Point(12, 54);
+            this.UserUpdatebutton.Name = "UserUpdatebutton";
+            this.UserUpdatebutton.Size = new System.Drawing.Size(110, 23);
+            this.UserUpdatebutton.TabIndex = 26;
+            this.UserUpdatebutton.Text = "Update";
+            this.UserUpdatebutton.UseVisualStyleBackColor = true;
+            this.UserUpdatebutton.Click += new System.EventHandler(this.UserUpdate);
+            // 
+            // UserInsertbutton
+            // 
+            this.UserInsertbutton.Location = new System.Drawing.Point(12, 25);
+            this.UserInsertbutton.Name = "UserInsertbutton";
+            this.UserInsertbutton.Size = new System.Drawing.Size(110, 23);
+            this.UserInsertbutton.TabIndex = 25;
+            this.UserInsertbutton.Text = "Insert";
+            this.UserInsertbutton.UseVisualStyleBackColor = true;
+            this.UserInsertbutton.Click += new System.EventHandler(this.UserInsert);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(758, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Current Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(632, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Phone Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(380, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Lastname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Surname";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Firstname";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(635, 28);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(120, 20);
+            this.textBox5.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(383, 28);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(257, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(761, 28);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(120, 20);
+            this.textBox6.TabIndex = 15;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(12, 83);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(110, 23);
+            this.searchButton.TabIndex = 30;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(12, 112);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(110, 23);
+            this.showAllButton.TabIndex = 31;
+            this.showAllButton.Text = "ShowAll";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.ShowAll);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(131, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(750, 446);
+            this.dataGridView1.TabIndex = 32;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 512);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.UserUpdatebutton);
+            this.Controls.Add(this.UserInsertbutton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button UserUpdatebutton;
+        private System.Windows.Forms.Button UserInsertbutton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ADOnet_ContactDataBase/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ADOnet_ContactDataBase
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = null;
+        SqlDataAdapter adapter = null;
+        DataSet dataSet = null;
+        string path = @"Server=DESKTOP-440KLQF\SQLEXPRESS;Database=ContactDataBase;Trusted_Connection=True;";
+        public Form1()
+        {
+            InitializeComponent();
+            Update();
+
+        }
+
+        private void UserInsert(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "")
+            {
+                SqlConnection conn = new SqlConnection(path);
+                try
+                {
+                    conn.Open();
+                    SqlCommand command = new SqlCommand(@"INSERT INTO [User](FirstName, SurName, LastName, BirthDay, PhoneNumber, AddedData) VALUES (@FirstName, @SecondName, @LastName, @Birthday, @PhoneNumber, @AddedData)", conn);
+                    command.Parameters.AddWithValue("FirstName", textBox1.Text);
+                    command.Parameters.AddWithValue("SecondName", textBox2.Text);
+                    command.Parameters.AddWithValue("LastName", textBox3.Text);
+                    command.Parameters.AddWithValue("Birthday", textBox4.Text);
+                    command.Parameters.AddWithValue("PhoneNumber", textBox5.Text);
+                    command.Parameters.AddWithValue("AddedData", textBox6.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Inserted in Database");
+                    FieldsClear();
+                }
+                catch (Exception)
+                {
+                    conn.Close();
+                    Update();
+                }
+            }
+        }
+
+
+        private void ShowAll(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Server=DESKTOP-440KLQF\SQLEXPRESS;Database=ContactDataBase;Trusted_Connection=True;");
+            conn.Open();
+            string command = "Select * from[User];";
+            SqlDataAdapter adapter = new SqlDataAdapter(command, conn);
+            dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            dataGridView1.DataSource = dataSet.Tables[0].DefaultView;
+        }
+        private void UserUpdate(object sender, EventArgs e)
+        {
+            Update();
+        }
+        private void Update()
+        {
+            SqlConnection conn = new SqlConnection(path);
+            try
+            {
+                conn.Open();
+                string command = "Select * from[User];";
+                SqlDataAdapter adapter = new SqlDataAdapter(command, conn);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                //dataGridView1.DataSource = dataSet.Tables[0].DefaultView;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void FieldsClear()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
+    }
+}
+            
